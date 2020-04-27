640b810b699256022cf6f588c7f155be6596502b
mobiusdonut <modularmercury@gmail.com>
2020-04-26 18:32:13 -0700

    fixed error where algo not accepting only exact matches

--- js/index.js
+++ js/index.js
@@ -56,9 +56,11 @@ $(document).ready(() => {
     }
     else {
         exact = []
-    } 
+    }
     keywords = filter.replace(/"([^"]+)"/g, '').replace(/"/g, '').split(' ').filter(function(word) { return !(word in stopword) && word.length > 3})
-    if (keywords.length > 0) {
+    console.log(keywords)
+    console.log(exact)
+    if (keywords.length > 0 || exact.length > 0) {
         for (let i = 0; i < li.length; i++) {
             a = li[i].getElementsByTagName("button")[0];
             txtValue = a.textContent || a.innerText;
